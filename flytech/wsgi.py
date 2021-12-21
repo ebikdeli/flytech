315f9d85a3870f4b744dfc196de319bbf74aaecb
ebikdeli <ebikdeli@gmail.com>
2021-12-21 18:01:04 +0330

    12/21/2021-18:00PM

--- flytech/wsgi.py
+++ flytech/wsgi.py
@@ -1,6 +1,7 @@
 """
 IMPORTANT: This file changed to support 'whitenoise' for testing purposes
 """
+"""
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
@@ -9,8 +10,8 @@ from django.conf import settings
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.BASE_DIR + '/assets')
 application.add_files(settings.BASE_DIR + '/assets', prefix='')
-
 """
+
 ### Ordinary wsgi file
 import os
 
@@ -19,4 +20,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flytech.settings.dev')
 
 application = get_wsgi_application()
-"""
